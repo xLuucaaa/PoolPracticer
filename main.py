5104794a00356cbc7d6b653cb332d87e29f2fc5e
lurait01 <lurait01@hs-esslingen.de>
2023-12-21 13:46:59 +0100

    display objects on the field

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pygame;
+import pygame
+import pymunk
+#to link the 2 libraries 
+import pymunk.pygame_util
+
 
 #initialize the pygame (modules, environment)
 pygame.init()
@@ -11,6 +15,30 @@ SCREEN_HEIGHT = 678
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pool Practier")
 
+
+#pymunk space
+space = pymunk.Space()
+#to tell pymunk to draw the shapes that we created, otherwise it is not getting displayed 
+draw_options = pymunk.pygame_util.DrawOptions(screen)
+
+#function for creating balls 
+#pymunk objects consist of body and shape (body is the center, shape is the surrounding)
+def create_ball(radius, position):
+    body = pymunk.Body()
+    body.position = position
+    shape = pymunk.Circle(body, radius)
+    #unitless value: experiment with is, to find the right mass
+    shape.mass = 5
+
+    space.add(body)
+    space.add(shape)
+    return shape
+
+newBall = create_ball(25, (300, 100))
+
+
+
+
 #game loop, so the screen stays displayed 
 run = True
 while run == True: 
@@ -18,5 +46,8 @@ while run == True:
         #pygame.QUIT is the X on the top right screen (^= closing the screen)
         if event.type == pygame.QUIT:
             run = False
+    
+    space.debug_draw(draw_options)
+    pygame.display.update()
 
 pygame.quit()
